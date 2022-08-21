420624c5d6d30c03380854bb961f96266c67142c
harishvanka <harishvanka@hotmail.com>
2022-08-21 23:18:10 +0530

    Added Getting exact recipe

--- hellp_api/app.py
+++ hellp_api/app.py
@@ -1,11 +1,36 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models import recipes
+from http import HTTPStatus
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
-    return "First API"
+    return 'First API'
+
+
+@app.route('/recipes', methods=['GET'])
+def get_all_recipies():
+    """
+    This method will return all recipies
+    :return: json data with all recipies
+    """
+    return jsonify(data=recipes)
+
+
+@app.route('/recipes/<int:recipe_id>')
+def get_recipe(recipe_id):
+    """
+    This method will return a recipe if  id is found else returns NOT FOUND
+    :param recipe_id:
+    :return: recipe if found else NOT_FOUND status
+    """
+    recipe_found = next((recipe for recipe in recipes if recipe['id'] == recipe_id), None)
+    if recipe_found:
+        return jsonify(recipe_found)
+    return jsonify(message='Recipe not found'), HTTPStatus.NOT_FOUND
+    pass
 
 
 if __name__ == '__main__':
---
+++ hellp_api/models.py
@@ -0,0 +1,12 @@
+recipes = [
+    {
+        'id': 1,
+        'name': 'Biryani',
+        'Description': 'This is wonderful biryani'
+    },
+    {
+        'id': 2,
+        'name': 'Icecream',
+        'Description': 'This is plain vanilla'
+    }
+]
